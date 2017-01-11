488eff90a2e0655318a9ce1342a80f7482dfe6ad
dscace <derek.scace@gmail.com>
2017-01-11 10:44:42 -0700

    Add items_by_category view

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,11 +1,13 @@
 class StorefrontController < ApplicationController
 	before_action :authenticate_user!
-	
+
   def all_items
   	@products = Product.all
   end
 
   def items_by_category
+  	@products = Product.where(category: params[:cat_id])
+  	@category = Category.find(params[:cat_id])
   end
 
   def items_by_brand
