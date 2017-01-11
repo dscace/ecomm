74192f61fd2c53319ed5e84cd5e657b2c42457a4
dscace <derek.scace@gmail.com>
2017-01-11 12:19:16 -0700

    Add user roles and admin abilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+     devise_parameter_sanitizer.permit(:account_update, keys[:role])
+  end
+  
   before_filter :categories, :brands
 
   def categories
