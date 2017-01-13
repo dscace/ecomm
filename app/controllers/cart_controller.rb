57080c9561fc8b688e828aec4e7da0892f516d3e
dscace <derek.scace@gmail.com>
2017-01-13 11:45:41 -0700

    Add badge to navbar cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,7 +4,7 @@ class CartController < ApplicationController
   def add_to_cart
 	  line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
 
-	  line_item.line_item_total = line_item.quantity * line_item.product.price
+	  line_item.line_item_total = 1 * line_item.product.price
 	  line_item.save
 
 	  redirect_to root_path
@@ -12,6 +12,22 @@ class CartController < ApplicationController
 
   def view_order
   	@line_items = LineItem.all
+  	@order = Order.create(user_id: current_user.id, subtotal: 0)
+
+	  @line_items.each do |line_item|
+	    @order.order_items[line_item.product_id] = line_item.quantity
+	    @order.subtotal += line_item.line_item_total
+	  end
+
+	  @order.sales_tax = @order.subtotal * 0.07
+	  @order.grand_total = @order.subtotal + @order.sales_tax
+	  @order.save
+
+	  @line_items.each do |line_item|
+	    line_item.quantity
+	    line_item.product.save
+	  end
+	  
   end
 
   def checkout
@@ -28,11 +44,12 @@ class CartController < ApplicationController
 	  @order.save
 
 	  @line_items.each do |line_item|
-	    line_item.product.quantity -= line_item.quantity
+	    line_item.quantity
 	    line_item.product.save
 	  end
 
 	  LineItem.destroy_all
+
 	end
 
 	def order_complete
@@ -54,5 +71,6 @@ class CartController < ApplicationController
     rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
+
 	end
 end
